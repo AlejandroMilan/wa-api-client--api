6325236ec0f1e54fdda909822d4ac8b22e3e8032
Alejandro Milan <alex.1306200des@gmail.com>
2025-09-20 03:18:05 -0600

    feat: Conversations and messages modules init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WaConversationsModule } from './wa-conversations/wa-conversations.module';
+import { WaMessagesModule } from './wa-messages/wa-messages.module';
 
 @Module({
-  imports: [],
+  imports: [WaConversationsModule, WaMessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
