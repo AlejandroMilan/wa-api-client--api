290192f2885f55fbbd9eee9d466304a30455a29b
Alejandro Milan <alex.1306200des@gmail.com>
2025-09-20 03:38:53 -0600

    feat: Config and Mongoose modules added to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WaConversationsModule } from './wa-conversations/wa-conversations.module';
 import { WaMessagesModule } from './wa-messages/wa-messages.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [WaConversationsModule, WaMessagesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        user: configService.get<string>('MONGO_USER'),
+        pass: configService.get<string>('MONGO_PASS'),
+        dbName: configService.get<string>('MONGO_DB'),
+      }),
+      inject: [ConfigService],
+    }),
+    WaConversationsModule,
+    WaMessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
